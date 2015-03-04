ffe8cb81f4d386c61a69f3e77293e9697b37c2f5
Mark Lamson <lamson_mark@yahoo.com>
2015-03-04 10:00:22 -0500

    form mess

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,19 @@
 function formBuilder(spec,$el){
   // BEGIN EDITING HERE
+  var i = spec;
+  console.log(spec);
+
+//if(spec === 1){
+
+  $form = $("<form></form>");
+  	$form.append('<h1>Comment Form</h1>');
+		$form.append('<input type="text" name="title" placeholder="Title"/><br/>');
+		$form.append('<input type="text" name="email"/><br/>');
+		$form.append('<input type="textbox" name="body"/><br/>');
+		$form.append('<input type="checkbox" name="subscribe" label="mail me when someone comments" /><br/>');
+		$form.append('<input type="button" value="button" />');
+		$('#spec').append($form);
+	//}
 
 
 
